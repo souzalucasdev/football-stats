4208161c947a8ab55cd090e3816800b7779d5d1d
Lucas <lucas.lfs.souza@gmail.com>
2025-06-13 18:29:53 +0200

    general fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,16 +10,6 @@ const anta = Anta({
   variable: '--font-anta',
 });
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
-
 export const metadata: Metadata = {
   title: 'Football Stats',
   description: '',
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   theme: {
     backgroundImage: {
       'field-lines': "url('/assets/field-marks.svg')",
